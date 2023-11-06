722acbb5a853b3ad234371c6e50c0240cd3c025d
Павлов Алексей Андреевич <alexpaa21@gmail.com>
2023-11-06 07:24:46 +0300

    Добавлены комментарии ко всем статусам

--- common/src/main/java/ru/liga/common/statuses/CourierStatus.java
+++ common/src/main/java/ru/liga/common/statuses/CourierStatus.java
@@ -1,5 +1,8 @@
 package ru.liga.common.statuses;
 
 public enum CourierStatus {
-    FREE, BUSY, REST
+
+    FREE, // Курьер свободен и ждет назначения ему заказа
+    BUSY, // Курьер доставляет заказ
+    REST // У курьера перерыв или выходной
 }
